44c65cce83607d0e07e120bb1acde4ba121a350d
Eric <ejwinebrenner@gmail.com>
2020-08-04 15:30:01 -0400

    db syntax fixed

--- db.js
+++ db.js
@@ -8,9 +8,9 @@ const Sequelize = require('sequelize');
 // });
 
 //! Heroku Deploy
-const sequelize = new Sequelize(process.env.HDB_URL), {
+const sequelize = new Sequelize(process.env.HDB_URL, {
     dialect: 'postgres'
-}
+})
 
 //*SPECIAL CHARACTER ENCODED URL
 // const sequelize = new Seuelize(process.env.HDB_URL || `postgresql://postgres:${encodeURIComponent(proess.env.PASS)}@localhost/kidcite`), {
